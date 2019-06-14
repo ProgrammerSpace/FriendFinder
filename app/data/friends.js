16098956ba980e96e23402ca76cb2c8066d0003f
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-06-13 23:08:00 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var dataList = [
+    {
+        "name": "Ahmed",
+        "photo": "https://images02.foap.com/images/0bdd91c4-6bb9-4e69-b3b6-7b0f94bb069a/funny-monkey-face.jpg?filename=w1280&dw=960",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://images02.foap.com/images/0bdd91c4-6bb9-4e69-b3b6-7b0f94bb069a/funny-monkey-face.jpg?filename=w1280&dw=960",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://images02.foap.com/images/0bdd91c4-6bb9-4e69-b3b6-7b0f94bb069a/funny-monkey-face.jpg?filename=w1280&dw=960",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://images02.foap.com/images/0bdd91c4-6bb9-4e69-b3b6-7b0f94bb069a/funny-monkey-face.jpg?filename=w1280&dw=960",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://images02.foap.com/images/0bdd91c4-6bb9-4e69-b3b6-7b0f94bb069a/funny-monkey-face.jpg?filename=w1280&dw=960",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5"
+        ]
+    }
+];
+
+module.exports = dataList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var list = require("../data/friends.js");
 
 var router1 = express.Router();
 
@@ -8,6 +9,26 @@ router1.get("/api/friendlist", function (req, res) {
 
 router1.get("/api/repo", function (req, res) {
     res.json("Repo");
+});
+
+router1.post("/api/findmatch", function (req, res) {
+    let compatibilityArray = [], compatibility = 0;
+    let newData = req.body;
+    console.log("new data: " + newData);
+    for (i in list) {
+        for (let j = 0; j < list[i].scores.length; j++) {
+            compatibility += Math.abs(list[i].scores[j] - newData.scores[j]);
+        }
+        compatibilityArray.push(parseInt(compatibility));
+        compatibility = 0;
+    }
+    console.log("compatibilityArray: " + compatibilityArray);
+    console.log("min: " + Math.min(...compatibilityArray));
+    let possibleMatch = compatibilityArray.indexOf(Math.min(...compatibilityArray));
+    console.log("possibleMatch: " + possibleMatch);
+    list.push(newData);
+    console.log(list);
+    res.json(list[possibleMatch]);
 });
 
 
